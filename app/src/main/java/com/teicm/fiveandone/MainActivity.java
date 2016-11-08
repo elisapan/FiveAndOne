a5e702bae520722a554928e3733c988f1c860b28
elisapan <lizpan94@outlook.com.gr>
2016-11-09 01:29:39 +0200

    Welcome page of the game.

--- app/src/main/java/com/teicm/fiveandone/MainActivity.java
+++ app/src/main/java/com/teicm/fiveandone/MainActivity.java
@@ -1,92 +1,47 @@
 package com.teicm.fiveandone;
 
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import static android.R.id.button1;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText usernameInput;
-    EditText passwordInput;
-    TextView infoText;
-    public Button button1;
-    public void init(){
-
-        button1 = (Button) findViewById(R.id.next);
-        button1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent act = new Intent(MainActivity.this, QuizActivity.class);
-                startActivity(act);
-
-            }
-        });
-
-
-    }
+    Button btnPlay, btnScore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        usernameInput = (EditText) findViewById(R.id.usernameInput) ;
-        passwordInput = (EditText) findViewById(R.id.passwordInput);
-        infoText = (TextView) findViewById(R.id.infoText);
-        //init();
+        btnPlay = (Button) findViewById(R.id.btnPlay);
+        btnScore = (Button) findViewById(R.id.btnScore);
 
-
-
-    }
-
-
-
-    //Save Info
-    public void saveInfo(View view){
-        SharedPreferences sharedPref = getSharedPreferences("userInfo", Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = sharedPref.edit();
-        editor.putString("username", usernameInput.getText().toString());
-        editor.putString("password", passwordInput.getText().toString());
-        editor.apply();
-
-        Toast.makeText(this, "Saved !", Toast.LENGTH_LONG).show();
-    }
-
-//Print the data
-
-
-    public void displayData(View view) {
-        SharedPreferences sharedPref = getSharedPreferences("userInfo", Context.MODE_PRIVATE);
-
-        final String name = sharedPref.getString("username", "");
-        String pw = sharedPref.getString("password", "");
-        infoText.setText("Username:" + name + " "+" Password:" + pw);
-
-        //
-        button1 = (Button) findViewById(R.id.next);
-        button1.setOnClickListener(new View.OnClickListener() {
+        btnPlay.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Intent act = new Intent(MainActivity.this, QuizActivity.class);
-                act.putExtra("parameter",name);
-                startActivity(act);
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),QuizActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
 
+        btnScore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),ScoreActivity.class);
+                startActivity(intent);
+                finish();
             }
         });
 
     }
-
-
-
-
-
 }
+
+
+
+
+
